1c21534fbb9d34c208c75f8bc5cce7bff0457095
Jonny84-max <jonnybest0684@gmail.com>
2026-01-18 16:57:05 +0100

    Improve readability of fuel_curve function
    Refactor fuel_curve function for readability.

--- propulsion_physics.py
+++ propulsion_physics.py
@@ -1,22 +1,20 @@
-"""
-Physics-based propulsion and fuel consumption model
-"""
-
 def resistance_increase(roughness, speed_kn):
+    # Simple resistance formula (example)
     return (1 + roughness * 10) * (speed_kn ** 2)
 
-def power_required(resistance, speed_kn):
-    speed_m_s = speed_kn * 0.51444
+def power_required(resistance, speed_kn):    speed_m_s = speed_kn * 0.51444
     return resistance * speed_m_s
 
 def speed_loss_due_to_fouling(roughness, speed_kn):
+    # Speed loss increases with roughness
     loss_percent = roughness * 50
     return speed_kn * (1 - loss_percent / 100)
 
 def fuel_consumption(power_kw):
+    # Simple fuel burn model (example)
     return power_kw * 0.2
 
 def fuel_curve(speed_kn, roughness):
-    resistance = resistance_increase(roughness, speed_kn)
-    power_kw = power_required(resistance, speed_kn) / 1000
+    res = resistance_increase(roughness, speed_kn)
+    power_kw = power_required(res, speed_kn) / 1000
     return fuel_consumption(power_kw)
