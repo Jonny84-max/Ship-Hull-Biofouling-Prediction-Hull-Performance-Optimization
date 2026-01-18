682f81feeb285b686a3cf73dce297a701762aae4
Jonny84-max <jonnybest0684@gmail.com>
2026-01-18 19:01:16 +0100

    Fix fuel consumption data in DataFrame

--- app.py
+++ app.py
@@ -95,5 +95,5 @@ fuel_values = [fuel_curve(speed, r) for r in fouling_range]
 
 df_fuel = pd.DataFrame({
     "Hull Roughness (mm)": fouling_range,
-    "Fuel Consumption (kg/hr)": fuel_consumption
+    "Fuel Consumption (kg/hr)": fuel_values,  # use the list you computed above
 })
