eefb2af97a239fb4ac3a1b41e023f1f9b9dbe200
Jonny84-max <jonnybest0684@gmail.com>
2026-01-19 12:02:08 +0100

    Update propulsion_physics.py

--- propulsion_physics.py
+++ propulsion_physics.py
@@ -1,18 +1,18 @@
 # Resistance due to hull fouling
 def resistance_increase(roughness, vessel_speed):
-    roughness: hull roughness (mm)
-    vessel_speed: speed in knots
+    # roughness: hull roughness (mm)
+    # vessel_speed: speed in knots
     return (1 + roughness * 15) * (vessel_speed ** 2)
 
 # Power required
 def power_required(resistance, vessel_speed):
-    resistance: abstract resistance unit
-    vessel_speed: knots
+    # resistance: abstract resistance unit
+    # vessel_speed: knots
     speed_m_s = vessel_speed * 0.51444
     return resistance * speed_m_s  # Watts (scaled)
 
 def speed_loss_due_to_fouling(roughness, vessel_speed):
-    Simple empirical fouling speed-loss model
+    # Simple empirical fouling speed-loss model
     # Prevent negative speed
     loss_percent = roughness * 50
     loss_percent = min(loss_percent, 90)    
@@ -20,17 +20,12 @@ def speed_loss_due_to_fouling(roughness, vessel_speed):
 
 # Fuel consumption (L/hr)
 def fuel_consumption_lph(power_kw, sfc=210, fuel_density=850):
-    power_kw: engine power in kW
-    sfc: specific fuel consumption (g/kWh)
-    fuel_density: kg/m3 (Marine Diesel Oil)
+    # power_kw: engine power in kW
+    # sfc: specific fuel consumption (g/kWh)
+    # fuel_density: kg/m3 (Marine Diesel Oil)
     fuel_kg_per_hr = (power_kw * sfc) / 1000
     fuel_lph = (fuel_kg_per_hr / fuel_density) * 1000
     return fuel_lph
-
-# Fuel consumption (tons/hr)
-def fuel_consumption_tph(power_kw, sfc=210):
-    fuel_kg_per_hr = (power_kw * sfc) / 1000
-    return fuel_kg_per_hr / 1000
 
 # Fuel curve vs fouling
 def fuel_curve(vessel_speed, roughness):
